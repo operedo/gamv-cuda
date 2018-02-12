5e26e56ae6ebb0d90dc8c0a2e5927aee5e95df05
Ubuntu <hdiuser@cities-gpu.y3vkgqhooedunalgt5jgymg34b.ex.internal.cloudapp.net>
2018-02-12 18:53:26 +0000

    Code cleaning

--- extractStatisticsCUDAwrapper.cu
+++ extractStatisticsCUDAwrapper.cu
@@ -11,14 +11,30 @@ Add description and legal texts
 #include <cuda_runtime.h>
 
 
-#define DT float
+#define DT double
 #define MAX(x,y)  ((x) >= (y) ? (x) : (y))
 #define MIN(x,y)  ((x) < (y) ? (x) : (y))
 #define MEM_OPTIMIZED 1
-#define THREADSX 16
+#define THREADSX 32
 #define THREADSY THREADSX
 
+#if !defined(__CUDA_ARCH__) || __CUDA_ARCH__ >= 600
 
+#else
+  static __inline__ __device__ double atomicAdd(double *address, double val) {
+    unsigned long long int* address_as_ull = (unsigned long long int*)address;
+    unsigned long long int old = *address_as_ull, assumed;
+    if (val==0.0)
+      return __longlong_as_double(old);
+    do {
+      assumed = old;
+      old = atomicCAS(address_as_ull, assumed, __double_as_longlong(val +__longlong_as_double(assumed)));
+    } while (assumed != old);
+    return __longlong_as_double(old);
+  }
+
+
+#endif
 
 void Check_CUDA_Error(const char *message)
 {
