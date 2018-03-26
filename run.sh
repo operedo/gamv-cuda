799cf4cf8210327601c14d6a2b58e7142b643fc1
Ubuntu <hdiuser@cities-gpu.y3vkgqhooedunalgt5jgymg34b.ex.internal.cloudapp.net>
2018-03-26 01:48:29 +0000

    Tests executed

--- run.sh
+++ run.sh
@@ -1,22 +1,23 @@
 
 #for example in sgsim_800x800x160 sisim_420x600x400 
-for example in sgsim_800x800x160 
-do
-	#for size in 15625 31250 62500 125000 250000 500000 1000000 2000000 4000000 8000000 
-	for size in 125000 
-	do
-		/usr/bin/time ./gamvCUDA.exe params/gamv-${example}_${size}.par > salida_${example}_${size}_cuda.txt 2>&1
-		#mv gamv.out gamv.out_${example}_${size}_cuda
-	done
-done
-
-#export OMP_NUM_THREADS=6
-#for example in sgsim_800x800x160 sisim_420x600x400 
+#for example in sgsim_800x800x160 
 #do
-#	for size in 15625 31250 62500 125000 250000 500000 1000000 2000000 4000000 8000000  
+#	#for size in 15625 31250 62500 125000 250000 500000 1000000 2000000 4000000 8000000 
+#	for size in 125000 
 #	do
-#		/usr/bin/time /home/hdiuser/dev/gslib-alges/par-gamv/src/gamv_OpenMP params/gamv-${example}_${size}_for.par > salida_openmp_${example}_${size}_fortran.txt 2>&1 
-#		/usr/bin/time ./gamvSeqFor.exe params/gamv-${example}_${size}_for.par > salida_${example}_${size}_fortran.txt 2>&1 
-#		mv gamv.out gamv.out_${example}_${size}_fortran  
+#		/usr/bin/time ./gamvCUDA.exe params/gamv-${example}_${size}.par > salida_${example}_${size}_cuda.txt 2>&1
+#		#mv gamv.out gamv.out_${example}_${size}_cuda
 #	done
 #done
+
+export OMP_NUM_THREADS=12
+for example in sgsim_800x800x160 sisim_420x600x400 
+do
+	#for size in 15625 31250 62500 125000 250000 500000 1000000 2000000 4000000 8000000  
+	for size in 250000 500000 1000000 2000000 4000000 8000000  
+	do
+		/usr/bin/time /home/hdiuser/dev/gslib-alges/par-gamv/src/gamv_OpenMP params/gamv-${example}_${size}_for.par > salida_openmp_${example}_${size}_12threads_fortran.txt 2>&1 
+		#/usr/bin/time ./gamvSeqFor.exe params/gamv-${example}_${size}_for.par > salida_${example}_${size}_fortran.txt 2>&1 
+		#mv gamv.out gamv.out_${example}_${size}_fortran  
+	done
+done
